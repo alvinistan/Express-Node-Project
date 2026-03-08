892a1381b7388368d0badab56941da4647743392
alvinistan <alvinistan@students.nsbm.ac.lk>
2026-03-08 21:46:43 +0530

    Express

---
+++ Lesson 3.1 — Express.js (First Express Server)/module 3 Express.md
@@ -0,0 +1,10 @@
+Module 3 — Express.js
+        Express is the most popular Node.js framework.
+
+It simplifies:
+
+        creating servers
+        routing
+        APIs
+        middleware
+        request handling
---
+++ Lesson 3.1 — Express.js (First Express Server)/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send('Home Page');
+})
+
+app.get('/about',(req,res)=> {
+    res.send('About page');
+})
+
+app.get('/contact', (req,res) => {
+    res.send('Contact page');   
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
